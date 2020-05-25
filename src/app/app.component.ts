31c0265b24c9edfda595f53f687e3e3eebc9a14d
sante stanisci <s.sante@hotmail.it>
2020-05-25 13:10:40 +0200

    fix css responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
-import {Component, HostListener} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {StickyService} from "./components/login-bar/StickyService";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private maxHeightOffset: number;
+
+  constructor(private stickyService: StickyService) { }
+
+  ngOnInit(): void {
+    this.stickyService.stickyEvent.subscribe((value: number) => this.maxHeightOffset = value);
+  }
+
   title = '';
 
   isSticky: boolean = false;
 
   @HostListener('window:scroll', ['$event'])
   checkScroll() {
-    this.isSticky = window.pageYOffset >= 50;
+    this.isSticky = window.pageYOffset >= this.maxHeightOffset;
   }
 }
