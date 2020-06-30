38bdf2bc1b6466eb5b846bbc620ae35fe01bc039
Giovanni Rubino <giovanni.rubino@linksmt.it>
2020-06-30 16:40:34 +0200

    verificaQuietanza, verificaEsitoPagamento service

--- src/app/modules/main/components/presaincaricopagamento/presaincaricopagamento.component.ts
+++ src/app/modules/main/components/presaincaricopagamento/presaincaricopagamento.component.ts
@@ -1,14 +1,43 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {PagamentoService} from "../../../../services/pagamento.service";
+import {interval} from "rxjs";
+
+const source = interval(10000);
 
 @Component({
   selector: 'app-presaincaricopagamento',
-  templateUrl:  './presaincaricopagamento.component.html',
+  templateUrl: './presaincaricopagamento.component.html',
   styleUrls: ['./presaincaricopagamento.component.scss']
 })
 export class PresaincaricopagamentoComponent implements OnInit {
 
-  constructor() { }
+  idSession: string;
+  timerId;
+  runCount = 0;
+
+  constructor(private route: ActivatedRoute, private pagamentoService: PagamentoService) {
+    this.route.queryParams.subscribe((params) => {
+      this.idSession = params.idSession;
+      this.pagamentoService.verificaQuietanza(this.idSession).subscribe();
+    });
+  }
 
   ngOnInit(): void {
+    // this.timerId = setInterval(() => {this.timerMethod();}, 10000);
+    this.timerId = source.subscribe(() => this.timerMethod());
   }
+
+  timerMethod(): any {
+    let ultima = false
+    this.runCount = this.runCount + 1;
+    if (this.runCount === 120) {
+      ultima = true
+      // clearInterval(this.timerId);
+      this.timerId.unsubscribe();
+    }
+    const observable = this.pagamentoService.verificaEsitoPagamento(ultima);
+    observable.subscribe();
+  }
+
 }
--- src/app/services/pagamento.service.ts
+++ src/app/services/pagamento.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams, HttpResponse} from '@angular/common/http';
 import {Observable, of} from 'rxjs';
 import {catchError, map} from 'rxjs/operators';
 import {XsrfService} from './xsrf.service';
@@ -13,6 +13,8 @@ import {Pagamento} from "../modules/main/model/Pagamento";
 export class PagamentoService {
 
   confermaPagamentoUrl = '/confermaPagamento';
+  verificaEsitoPagamentoUrl = '/verificaEsitoPagamento';
+  verificaQuietanzaUrl = '/verificaQuietanza';
 
   constructor(private http: HttpClient, private xsrfService: XsrfService) {
   }
@@ -30,5 +32,36 @@ export class PagamentoService {
           return caught;
       }));
   }
+
+  public verificaQuietanza(idSession: string): Observable<HttpResponse<any>> {
+    return this.http.post(environment.bffBaseUrl + this.verificaQuietanzaUrl, idSession, {observe: "response"})
+      .pipe(map((response: HttpResponse<any>) => {
+        const headers: HttpHeaders = response.headers;
+        this.xsrfService.xsrfToken = headers.get('XSRF-TOKEN');
+        return response;
+      }), catchError((err, caught) => {
+        if (err.status == 401) {
+          return of(null);
+        } else
+          return caught;
+      }));
+  }
+
+  public verificaEsitoPagamento(ultima: boolean): Observable<any> {
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append("XSRF-TOKEN", this.xsrfService.xsrfToken)
+    // const params = new HttpParams();
+    //     // params.set('ultima', String(ultima));
+    return this.http.post(environment.bffBaseUrl + this.verificaEsitoPagamentoUrl, ultima,{headers: headers})
+      .pipe(map((json: any) => {
+        return json;
+      }), catchError((err, caught) => {
+        if (err.status == 401) {
+          return of(null);
+        } else
+          return caught;
+      }));
+  }
+
 
 }
