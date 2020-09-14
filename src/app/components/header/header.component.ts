52e4cf633e2fee88cd3f341d2dfde0284a98e440
Ivan Lamparelli <ivan.lamparelli@linksmt.it>
2020-09-14 17:57:45 +0200

    added dynamic show/hide logic for cart icon

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import {Component, HostListener, OnInit} from '@angular/core';
 import {StickyService} from '../login-bar/StickyService';
+import {NavigationEnd, Router} from '@angular/router';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -9,13 +11,18 @@ import {StickyService} from '../login-bar/StickyService';
 export class HeaderComponent implements OnInit {
   private maxHeightOffset: number;
 
-  constructor(private stickyService: StickyService) { }
+  constructor(private stickyService: StickyService, private router: Router) { }
 
   ngOnInit(): void {
     this.stickyService.stickyEvent.subscribe((value: number) => this.maxHeightOffset = value);
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(event => {
+      this.isPaginaNuovoPagamento = window.location.pathname === this.urlNuovoPagamento;
+    })
   }
 
   isSticky: boolean = false;
+  urlNuovoPagamento = "/nuovopagamento";
+  isPaginaNuovoPagamento: boolean = window.location.pathname === this.urlNuovoPagamento;
 
   @HostListener('window:scroll', ['$event'])
   checkScroll() {
