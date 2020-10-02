34d362da2790df1a505b7db83ba10acd465d63d2
Giovanni Rubino <giovanni.rubino@dxc.com>
2020-10-02 14:54:09 +0200

    delete test confermaPagamento

--- src/app/modules/main/components/home/home.component.ts
+++ src/app/modules/main/components/home/home.component.ts
@@ -40,30 +40,6 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // todo test confermaPagamento
-    const chiamaConfermaPagamento = false; // cambiare a true per testare
-    const email = 'giovanni.rubino@dxc.com'
-    const bollettino = new Bollettino();
-    bollettino.anno = 2020;
-    bollettino.causale = 'causale di prova';
-    bollettino.cfpiva = 'RBNGNN94C11A662V';
-    bollettino.enteId = 1;
-    bollettino.servizioId = 1;
-    bollettino.importo = 90.5;
-    bollettino.iuv = 'LEP12345';
-    bollettino.numero = 'NUM987';
-    const listaCampoDettaglioTransazione: CampoDettaglioTransazione[] = []
-    const campoDettaglioTransazione = new CampoDettaglioTransazione();
-    campoDettaglioTransazione.titolo = 'titolo';
-    campoDettaglioTransazione.valore = 'valore';
-    listaCampoDettaglioTransazione.push(campoDettaglioTransazione)
-    bollettino.listaCampoDettaglioTransazione = listaCampoDettaglioTransazione;
-    const body: Bollettino[] = [];
-    body.push(bollettino);
-    if (chiamaConfermaPagamento) {
-      this.pagamentoService.confermaPagamento(body, email).subscribe();
-    }
-
     const bannerObservable: Observable<Banner[]> = this.bannerService
       .letturaBanner(this.bannerService.timestamp, this.bannerService.attivo);
     bannerObservable.subscribe((banners) => {
