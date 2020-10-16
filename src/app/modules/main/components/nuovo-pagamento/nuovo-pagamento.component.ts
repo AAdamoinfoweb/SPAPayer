e6d1ebe69b6d332b3298a8d29191d74e986d9493
Ivan Lamparelli <lamparelli.ivan@gmail.com>
2020-10-16 13:49:01 +0200

    refactored breadcrumb and title logic to match MieiPagamenti

--- src/app/modules/main/components/nuovo-pagamento/nuovo-pagamento.component.ts
+++ src/app/modules/main/components/nuovo-pagamento/nuovo-pagamento.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {FiltroServizio} from '../../model/FiltroServizio';
 import {NuovoPagamentoService} from '../../../../services/nuovo-pagamento.service';
 import {DatiPagamento} from '../../model/bollettino/DatiPagamento';
+import {Breadcrumb} from '../../dto/Breadcrumb';
 
 @Component({
   selector: 'app-nuovo-pagamento',
@@ -10,6 +11,8 @@ import {DatiPagamento} from '../../model/bollettino/DatiPagamento';
 })
 export class NuovoPagamentoComponent implements OnInit {
   servizioSelezionato: FiltroServizio;
+  breadcrumbList = [];
+  titolo: string;
 
   @Input()
   datiPagamento: DatiPagamento;
@@ -21,6 +24,19 @@ export class NuovoPagamentoComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.inizializzaHeaderSezione();
+  }
+
+  inizializzaHeaderSezione(): void {
+    if (this.datiPagamento) {
+      this.titolo = 'Dettaglio Pagamento';
+      this.breadcrumbList.push(new Breadcrumb(0, '< I Miei Pagamenti', '/iMieiPagamenti', null));
+    } else {
+      this.titolo = 'Nuovo Pagamento';
+      this.breadcrumbList.push(new Breadcrumb(0, 'Home', null, null));
+      this.breadcrumbList.push(new Breadcrumb(1, 'Pagamenti', null, null));
+      this.breadcrumbList.push(new Breadcrumb(1, 'Nuovo Pagamento', null, null));
+    }
   }
 
 }
