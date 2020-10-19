d2e285c6991d8d4c87d487dc80659bff11dde977
s <s.sante@hotmail.it>
2020-10-19 19:30:03 +0200

    fix log azione

--- src/app/modules/main/components/amministrativo/amministrativo-parent.component.ts
+++ src/app/modules/main/components/amministrativo/amministrativo-parent.component.ts
@@ -4,6 +4,8 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {environment} from "../../../../../environments/environment";
 import {OverlayService} from "../../../../services/overlay.service";
 import {AmministrativoService} from "../../../../services/amministrativo.service";
+import {flatMap, map} from 'rxjs/operators';
+import {Observable, of} from "rxjs";
 
 @Component({
   selector: 'base-amm',
@@ -13,25 +15,38 @@ export class AmministrativoParentComponent implements OnInit {
 
   public waitingEmitter: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+  private inserimentoLogAzioneUrl = '/inserimentoLogAzione';
+
   constructor(
     protected router: Router,
     protected  overlayService: OverlayService,
     protected  route: ActivatedRoute, private http: HttpClient,
     protected amministrativoService: AmministrativoService) {
-      this.overlayService.caricamentoEvent.emit(true);
-      this.waitingEmitter.emit(true);
-      this.route.queryParams.subscribe(value => {
-        this.amministrativoService.idFunzione =  atob(value.funzione);
-        let h: HttpHeaders = new HttpHeaders();
-        h = h.append('idFunzione', this.amministrativoService.idFunzione);
-        this.http.get(environment.bffBaseUrl + '/verificaAbilitazione', {headers: h, withCredentials: true}).subscribe(() => {
-          this.overlayService.caricamentoEvent.emit(false);
-          this.waitingEmitter.emit(false);
-        });
-      });
+    this.overlayService.caricamentoEvent.emit(true);
+    this.waitingEmitter.emit(true);
+    this.route.queryParams.subscribe(value => {
+      this.amministrativoService.idFunzione = atob(value.funzione);
+      let h: HttpHeaders = new HttpHeaders();
+      h = h.append('idFunzione', this.amministrativoService.idFunzione);
+      let observable: Observable<any> = this.http.get(environment.bffBaseUrl + '/verificaAbilitazione', {
+        headers: h,
+        withCredentials: true
+      }).pipe(map(() => {
+        this.overlayService.caricamentoEvent.emit(false);
+        this.waitingEmitter.emit(false);
+        this.inserimentoLogAzione().subscribe();
+        return of(null);
+      }));
+      observable.subscribe();
+    });
   }
 
   ngOnInit(): void {
+  }
 
+  private inserimentoLogAzione(): Observable<any> {
+    return this.http.post(environment.bffBaseUrl + this.inserimentoLogAzioneUrl, this.amministrativoService.idFunzione, {
+      withCredentials: true
+    });
   }
 }
