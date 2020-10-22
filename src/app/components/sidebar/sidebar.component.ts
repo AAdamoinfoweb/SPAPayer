0293fad8d5b3958c7c3bf708a3313a0d238a2eb3
s <s.sante@hotmail.it>
2020-10-22 17:50:47 +0200

    for merge master

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -74,8 +74,8 @@ export class SidebarComponent implements OnInit {
     if (item.route) {
       if (item.nome === 'Accedi') {
         localStorage.setItem('loginDaAnonimo', 'true');
-        //window.location.href = environment.bffBaseUrl + item.route;
-        window.location.href = "http://service.pp.192-168-43-56.nip.io/api/loginLepida.htm?CodiceFiscale=STNSNT85T11C975I&nome=sante&cognome=sta&email=sante.stanisci@dxc.com";
+        window.location.href = environment.bffBaseUrl + item.route;
+        //window.location.href = "http://service.pp.192-168-43-56.nip.io/api/loginLepida.htm?CodiceFiscale=STNSNT85T11C975A&nome=sante&cognome=sta&email=sante.stanisci@dxc.com";
       } else if (item.nome === 'Esci') {
         this.http.get(environment.bffBaseUrl + '/logout', {withCredentials: true}).subscribe((body: any) => {
           if (body.url) {
