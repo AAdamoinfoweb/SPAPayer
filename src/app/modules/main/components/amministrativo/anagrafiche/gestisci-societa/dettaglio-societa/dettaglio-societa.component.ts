4507deefbe8c67dc2fb5fbbff46d1c8bf51d4974
Ivan Lamparelli <lamparelli.ivan@gmail.com>
2020-10-23 10:51:19 +0200

    fixed logic for add societa

--- src/app/modules/main/components/amministrativo/anagrafiche/gestisci-societa/dettaglio-societa/dettaglio-societa.component.ts
+++ src/app/modules/main/components/amministrativo/anagrafiche/gestisci-societa/dettaglio-societa/dettaglio-societa.component.ts
@@ -98,9 +98,8 @@ export class DettaglioSocietaComponent implements OnInit {
     switch (this.funzione) {
       case FunzioneGestioneEnum.AGGIUNGI:
         this.societaService.aggiuntaSocieta(this.societa, this.amministrativoService.idFunzione).subscribe((societa) => {
-          // TODO attendere fix backend e ritestare (al momento il backend restituisce void, e non è possibile recuperare l'id)
           this.overlayService.caricamentoEvent.emit(false);
-          this.router.navigate(['/modificaSocieta', societa.id]);
+          this.societa = new Societa();
         });
         break;
       case FunzioneGestioneEnum.MODIFICA:
