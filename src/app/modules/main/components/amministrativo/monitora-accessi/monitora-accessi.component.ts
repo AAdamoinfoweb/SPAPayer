3003697166b3a3f2e1af3a8c24669d723310c747
Ivan Lamparelli <lamparelli.ivan@gmail.com>
2020-10-27 18:23:57 +0100

    added logic for retrieval of accessi from backend

--- src/app/modules/main/components/amministrativo/monitora-accessi/monitora-accessi.component.ts
+++ src/app/modules/main/components/amministrativo/monitora-accessi/monitora-accessi.component.ts
@@ -1,15 +1,103 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {GestisciElementoComponent} from '../gestisci-elemento.component';
+import {ToolEnum} from '../../../../../enums/Tool.enum';
+import {Colonna} from '../../../model/tabella/Colonna';
+import {ImmaginePdf} from '../../../model/tabella/ImmaginePdf';
+import {ActivatedRoute, Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {AmministrativoService} from '../../../../../services/amministrativo.service';
+import {SocietaService} from '../../../../../services/societa.service';
+import {MenuService} from '../../../../../services/menu.service';
+import {ConfirmationService} from 'primeng/api';
 
 @Component({
   selector: 'app-monitora-accessi',
   templateUrl: './monitora-accessi.component.html',
   styleUrls: ['./monitora-accessi.component.scss']
 })
-export class MonitoraAccessiComponent implements OnInit {
+export class MonitoraAccessiComponent extends GestisciElementoComponent implements OnInit {
 
-  constructor() { }
+  isMenuCarico: boolean = false;
+
+  constructor(router: Router,
+              route: ActivatedRoute, http: HttpClient, amministrativoService: AmministrativoService,
+              private renderer: Renderer2, private el: ElementRef,
+              private menuService: MenuService,
+              private confirmationService: ConfirmationService
+  ) {
+    super(router, route, http, amministrativoService);
+  }
 
   ngOnInit(): void {
+    this.waitingEmitter.subscribe(() => {
+
+      if (this.amministrativoService.mappaFunzioni) {
+        this.isMenuCarico = Object.keys(this.amministrativoService.mappaFunzioni).length > 0;
+      }
+
+      if (this.isMenuCarico) {
+        this.init();
+      } else {
+        this.menuService.menuCaricatoEvent.subscribe(() => {
+          this.init();
+        });
+      }
+    });
+  }
+
+  init(): void {
+    this.inizializzaBreadcrumbList([
+      {label: 'Monitora Accessi', link: null}
+    ]);
+    this.popolaListaElementi();
+  }
+
+  creaRigaTabella(oggetto: any) {
+    // TODO implementare metodo
+  }
+
+  eseguiAzioni(azioneTool: ToolEnum): void {
+    // TODO implementare metodo
+  }
+
+  getColonneFileExcel(colonne: Colonna[]): Colonna[] {
+    // TODO implementare metodo
+    return [];
+  }
+
+  getColonneFilePdf(colonne: Colonna[]): Colonna[] {
+    // TODO implementare metodo
+    return [];
+  }
+
+  getImmaginiFilePdf(): ImmaginePdf[] {
+    // TODO implementare metodo
+    return [];
+  }
+
+  getNumeroRecord(): string {
+    // TODO implementare metodo
+    return '';
+  }
+
+  getRigheFileExcel(righe: any[]) {
+    // TODO implementare metodo
+  }
+
+  getRigheFilePdf(righe: any[]) {
+    // TODO implementare metodo
+  }
+
+  onChangeListaElementi(listaElementi: any[]): void {
+    // TODO implementare metodo
+  }
+
+  popolaListaElementi(): void {
+
+  }
+
+  selezionaRigaTabella(righeSelezionate: any[]): void {
+    // TODO implementare metodo
   }
 
 }
---
+++ src/app/modules/main/model/accesso/ParametriRicercaAccesso.ts
@@ -0,0 +1,7 @@
+export class ParametriRicercaAccesso {
+  funzioneId: number = null;
+  codiceFiscale: string = null;
+  indirizzoIP: string = null;
+  inizioSessione: Date = null;
+  fineSessione: Date = null;
+}
