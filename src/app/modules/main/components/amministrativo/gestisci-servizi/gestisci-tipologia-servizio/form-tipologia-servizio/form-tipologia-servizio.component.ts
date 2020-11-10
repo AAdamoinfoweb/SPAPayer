72b2a4b9af4d835793adc60932815729bd8ee84d
sante stanisci <s.sante@hotmail.it>
2020-11-10 17:44:31 +0100

    clear method

--- src/app/modules/main/components/amministrativo/gestisci-servizi/gestisci-tipologia-servizio/form-tipologia-servizio/form-tipologia-servizio.component.ts
+++ src/app/modules/main/components/amministrativo/gestisci-servizi/gestisci-tipologia-servizio/form-tipologia-servizio/form-tipologia-servizio.component.ts
@@ -43,6 +43,8 @@ export class FormTipologiaServizioComponent extends FormElementoParentComponent 
   readonly lunghezzaMaxCol2: number = 10;
   readonly lunghezzaMaxCol3: number = 15;
 
+  showEditId: number;
+
   constructor(
     protected activatedRoute: ActivatedRoute,
     protected router: Router,
@@ -57,10 +59,6 @@ export class FormTipologiaServizioComponent extends FormElementoParentComponent 
   }
 
   ngAfterViewInit() {
-    /*let phElement = this.placeholder.element.nativeElement;
-
-    phElement.style.display = 'none';
-    phElement.parentElement.removeChild(phElement);*/
   }
 
   initFormPage(snapshot: ActivatedRouteSnapshot) {
@@ -79,91 +77,6 @@ export class FormTipologiaServizioComponent extends FormElementoParentComponent 
     let campoForm = new CampoForm();
     campoForm.titolo = "nuovo campo";
     this.items.push(campoForm);
-  }
-
-
-  dragMoved(e: CdkDragMove) {
-    const point = this.getPointerPositionOnPage(e.event);
-
-    this.listGroup._items.forEach(dropList => {
-      if (__isInsideDropListClientRect(dropList, point.x, point.y)) {
-        this.activeContainer = dropList;
-        return;
-      }
-    });
-  }
-
-  dropListDropped(event: any) {
-    if (!this.target) {
-      return;
-    }
-    /*
-        const phElement = this.placeholder.element.nativeElement;
-        const parent = phElement.parentElement;
-
-        let clone = _.cloneDeep(phElement);
-        phElement.style.display = 'none';
-
-        parent.removeChild(phElement);
-        parent.appendChild(clone);
-        parent.insertBefore(this.source.element.nativeElement, parent.children[this.sourceIndex]);
-    */
-    this.target = null;
-    this.source = null;
-
-    if (this.sourceIndex !== this.targetIndex) {
-      moveItemInArray(this.items, this.sourceIndex, this.targetIndex);
-      this.items = _.cloneDeep(this.items);
-    }
-  }
-
-  dropListEnterPredicate = (drag: CdkDrag, drop: CdkDropList) => {
-    /* if (drop == this.placeholder) {
-       return true;
-     }
-
-     if (drop != this.activeContainer) {
-       return false;
-     }*/
-
-    const phElement = this.placeholder.element.nativeElement;
-    const sourceElement = drag.dropContainer.element.nativeElement;
-    const dropElement = drop.element.nativeElement;
-
-    const dragIndex = __indexOf(dropElement.parentElement.children, (this.source ? phElement : sourceElement));
-    const dropIndex = __indexOf(dropElement.parentElement.children, dropElement);
-
-    if (!this.source) {
-      this.sourceIndex = dragIndex;
-      this.source = drag.dropContainer;
-
-      phElement.style.width = sourceElement.clientWidth + 'px';
-      phElement.style.height = sourceElement.clientHeight + 'px';
-
-      sourceElement.parentElement.removeChild(sourceElement);
-    }
-
-    this.targetIndex = dropIndex;
-    this.target = drop;
-
-    phElement.style.display = '';
-    dropElement.parentElement.insertBefore(phElement, (dropIndex > dragIndex
-      ? dropElement.nextSibling : dropElement));
-
-    this.placeholder._dropListRef.enter(drag._dragRef, drag.element.nativeElement.offsetLeft, drag.element.nativeElement.offsetTop);
-    return false;
-  }
-
-  /** Determines the point of the page that was touched by the user. */
-  getPointerPositionOnPage(event: MouseEvent | TouchEvent) {
-    // `touches` will be empty for start/end events so we have to fall back to `changedTouches`.
-    const point = __isTouchEvent(event) ? (event.touches[0] || event.changedTouches[0]) : event;
-    const scrollPosition = this.viewportRuler.getViewportScrollPosition();
-
-    return {
-      x: point.pageX - scrollPosition.left,
-      y: point.pageY - scrollPosition.top
-    };
   }
 
   removeItem(item: CampoForm) {
@@ -191,36 +104,8 @@ export class FormTipologiaServizioComponent extends FormElementoParentComponent 
     return classe;
   }
 
-  activeNumIndex: number;
-
-  enter(i) {
-    this.activeNumIndex = i;
-  }
-
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
-  }
-
-  drop1(event: CdkDragDrop<CampoForm[], any>) {
-    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
-  }
-
-  drop2(event: CdkDragDrop<{ item: CampoForm; index: number }, any>) {
+  dropEvt(event: CdkDragDrop<{ item: CampoForm; index: number }, any>) {
     this.items[event.previousContainer.data.index] = event.container.data.item;
     this.items[event.container.data.index] = event.previousContainer.data.item;
   }
-}
-
-function __indexOf(collection, node) {
-  return Array.prototype.indexOf.call(collection, node);
-}
-
-/** Determines whether an event is a touch event. */
-function __isTouchEvent(event: MouseEvent | TouchEvent): event is TouchEvent {
-  return event.type.startsWith('touch');
-}
-
-function __isInsideDropListClientRect(dropList: CdkDropList, x: number, y: number) {
-  const {top, bottom, left, right} = dropList.element.nativeElement.getBoundingClientRect();
-  return y >= top && y <= bottom && x >= left && x <= right;
 }
