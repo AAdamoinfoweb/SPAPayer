f8ce72371d7ca30dd72adcbce0f08e0f2ec99079
Ivan Lamparelli <lamparelli.ivan@gmail.com>
2020-11-13 14:14:59 +0100

    aggiunto model Configuratore

---
+++ src/app/modules/main/model/campo/ConfigurazioneCampoDettaglioTransazione.ts
@@ -0,0 +1,3 @@
+export class ConfigurazioneCampoDettaglioTransazione {
+  nome: string;
+}
---
+++ src/app/modules/main/model/campo/ConfigurazioneJsonPath.ts
@@ -0,0 +1,8 @@
+export class ConfigurazioneJsonPath {
+  id: number;
+  nome_attributo: string;
+  json_path: string;
+  campo_input: boolean;
+  livello_integrazione_id: number;
+  tipologia_bollettino: string;
+}
