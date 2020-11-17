7c8736d69019a519aeb0e28347b74002f305db05
Gianfranco Brescia <gbrescia70@gmail.com>
2020-11-17 18:11:12 +0100

    inizializzato componente gestisciAttivitaPianificate

---
+++ src/app/modules/main/components/amministrativo/gestisci-attivita-pianificate/gestisci-attivita-pianificate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestisci-attivita-pianificate',
+  templateUrl: './gestisci-attivita-pianificate.component.html',
+  styleUrls: ['./gestisci-attivita-pianificate.component.scss']
+})
+export class GestisciAttivitaPianificateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
