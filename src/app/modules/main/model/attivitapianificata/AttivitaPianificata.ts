f282d93cb234f3d85a704fef4d5088c1499876c6
Giovanni Rubino <giovanni.rubino@dxc.com>
2020-11-18 14:01:47 +0100

    attività pianificata service e model

--- src/app/modules/main/model/attivitapianificata/AttivitaPianificata.ts
+++ src/app/modules/main/model/attivitapianificata/AttivitaPianificata.ts
@@ -1,9 +1,16 @@
 import {Schedulazione} from "../schedulazione/Schedulazione";
+import {ParametroAttivitaPianificata} from "./ParametroAttivitaPianificata";
 
-export class AttivitaPianificata{
+export class AttivitaPianificata {
   id: number;
   titolo: string;
   descrizione: string;
   abilitato: boolean;
   schedulazione: Schedulazione;
+  parametri: ParametroAttivitaPianificata[];
+
+  constructor() {
+    this.parametri = [];
+    this.schedulazione = new Schedulazione();
+  }
 }
---
+++ src/app/services/attivita-pianificata.service.ts
@@ -0,0 +1,135 @@
+import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {environment} from '../../environments/environment';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {catchError, map} from 'rxjs/operators';
+import {ParametriRicercaStatistiche} from "../modules/main/model/statistica/ParametriRicercaStatistiche";
+import {Statistica} from "../modules/main/model/statistica/Statistica";
+import {AttivitaPianificata} from "../modules/main/model/attivitapianificata/AttivitaPianificata";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AttivitaPianificataService {
+
+  private readonly gestisciAttivitaPianificateBasePath = '/gestisciAttivitaPianificate';
+
+  private readonly attivitaPianificataBaseUrl = this.gestisciAttivitaPianificateBasePath + '/attivitaPianificate';
+
+  private readonly eliminaAttivitaPianificateUrl = this.gestisciAttivitaPianificateBasePath + '/eliminaAttivitaPianificate';
+
+  constructor(private http: HttpClient) {
+  }
+
+  ricercaAttivitaPianificate(parametriRicercaStatistiche: ParametriRicercaStatistiche, idFunzione: string): Observable<any[]> {
+    const url = environment.bffBaseUrl + this.attivitaPianificataBaseUrl;
+    // set headers
+    let h: HttpHeaders = new HttpHeaders();
+    h = h.append('idFunzione', idFunzione);
+    // set params
+    let params = new HttpParams();
+    if (parametriRicercaStatistiche.attiva != null) {
+      params = params.set('attiva', String(parametriRicercaStatistiche.attiva));
+    }
+    if (parametriRicercaStatistiche.avvioSchedulazione) {
+      params = params.set('avvioSchedulazione', parametriRicercaStatistiche.avvioSchedulazione);
+    }
+    if (parametriRicercaStatistiche.fineSchedulazione) {
+      params = params.set('fineSchedulazione', parametriRicercaStatistiche.fineSchedulazione);
+    }
+
+    return this.http.get(`${url}`,
+      {
+        withCredentials: true,
+        headers: h,
+        params
+      }).pipe(map((body: any[]) => {
+        return body;
+      }),
+      catchError((err, caught) => {
+        if (err.status === 401 || err.status === 400) {
+          return of(null);
+        } else {
+          return of(null);
+        }
+      }));
+  }
+
+  eliminaAttivitaPianificate(listaStatisticheId: Array<number>, idFunzione: string): Observable<any> {
+    const url = environment.bffBaseUrl + this.eliminaAttivitaPianificateUrl;
+    let h: HttpHeaders = new HttpHeaders();
+    h = h.append('idFunzione', idFunzione);
+
+    return this.http.post(`${url}`, listaStatisticheId,
+      {
+        withCredentials: true,
+        headers: h
+      }).pipe(map((body: any) => {
+      return body;
+    }));
+  }
+
+  inserimentoAttivitaPianificata(attivitaPianificata: AttivitaPianificata, idFunzione: string): Observable<number> {
+    const url = environment.bffBaseUrl + this.attivitaPianificataBaseUrl;
+    let h: HttpHeaders = new HttpHeaders();
+    h = h.append('idFunzione', idFunzione);
+
+    return this.http.post(`${url}`, attivitaPianificata,
+      {
+        withCredentials: true,
+        headers: h
+      }).pipe(map((body: number) => {
+        return body;
+      }),
+      catchError((err, caught) => {
+        if (err.status === 401 || err.status === 400) {
+          return of(null);
+        } else {
+          return of(null);
+        }
+      }));
+  }
+
+  modificaAttivitaPianificata(attivitaPianificata: AttivitaPianificata, idFunzione: string): Observable<number> {
+    const url = environment.bffBaseUrl + this.attivitaPianificataBaseUrl + '/' + attivitaPianificata.id;
+    let h: HttpHeaders = new HttpHeaders();
+    h = h.append('idFunzione', idFunzione);
+
+    return this.http.put(`${url}`, attivitaPianificata,
+      {
+        withCredentials: true,
+        headers: h
+      }).pipe(map((body: number) => {
+        return body;
+      }),
+      catchError((err, caught) => {
+        if (err.status === 401 || err.status === 400) {
+          return of(null);
+        } else {
+          return of(null);
+        }
+      }));
+  }
+
+  dettaglioAttivitaPianificata(idAttivitaPianificata: number, idFunzione: string): Observable<AttivitaPianificata> {
+    const url = environment.bffBaseUrl + this.attivitaPianificataBaseUrl + '/' + idAttivitaPianificata;
+    let h: HttpHeaders = new HttpHeaders();
+    h = h.append('idFunzione', idFunzione);
+
+    return this.http.get(`${url}`,
+      {
+        withCredentials: true,
+        headers: h
+      }).pipe(map((body: Statistica) => {
+        return body;
+      }),
+      catchError((err, caught) => {
+        if (err.status === 401 || err.status === 400) {
+          return of(null);
+        } else {
+          return of(null);
+        }
+      }));
+  }
+
+}
