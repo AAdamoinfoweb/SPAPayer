4e9e7b603c12f6c17d8d7072d98d74ccde4f51f4
Ivan Lamparelli <lamparelli.ivan@gmail.com>
2020-11-20 12:28:27 +0100

    convertito campo nome della modale aggiungi tipo campo da input a select

--- src/app/modules/main/components/amministrativo/gestisci-servizi/gestisci-tipologia-servizio/modale-campo-form/modale-aggiungi-tipo-campo/modale-aggiungi-tipo-campo.component.ts
+++ src/app/modules/main/components/amministrativo/gestisci-servizi/gestisci-tipologia-servizio/modale-campo-form/modale-aggiungi-tipo-campo/modale-aggiungi-tipo-campo.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {OverlayService} from "../../../../../../../../services/overlay.service";
 import {CampoTipologiaServizioService} from "../../../../../../../../services/campo-tipologia-servizio.service";
+import {TipoCampo} from '../../../../../../model/campo/TipoCampo';
 
 @Component({
   selector: 'app-modale-aggiungi-tipo-campo',
@@ -13,6 +14,7 @@ export class ModaleAggiungiTipoCampoComponent implements OnInit {
   form: FormGroup;
   nome: string;
   informazioni: string;
+  listaNomi: string[];
 
   @Input()
   idFunzione: number;
@@ -22,6 +24,14 @@ export class ModaleAggiungiTipoCampoComponent implements OnInit {
       nome: new FormControl(null, [Validators.required]),
       informazioni: new FormControl(null, [Validators.required])
     });
+
+    const listaTipiCampo: TipoCampo[] = JSON.parse(localStorage.getItem('listaTipiCampo'));
+    if (listaTipiCampo) {
+      const listaNomi: string[] = listaTipiCampo.map(tipo => tipo.nome);
+      const listaNomiUnivoci: string[] = listaNomi.filter((nome, index) => listaNomi.lastIndexOf(nome) === index);
+      listaNomiUnivoci.sort();
+      this.listaNomi = listaNomiUnivoci;
+    }
   }
 
 
