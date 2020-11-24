7048472796c95cef93ead2d577b7b1689afd2c6f
Gianfranco Brescia <gbrescia70@gmail.com>
2020-11-24 18:01:32 +0100

    aggiunte checkbox per funzioni spuntando quelle abilitate in dettaglio monitora accessi

--- src/app/modules/main/components/amministrativo/monitora-accessi/dati-accesso/dati-accesso.component.ts
+++ src/app/modules/main/components/amministrativo/monitora-accessi/dati-accesso/dati-accesso.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core'
 import {Accesso} from '../../../../model/accesso/Accesso';
 import {Utils} from '../../../../../../utils/Utils';
 import * as moment from 'moment';
+import {FunzioneService} from '../../../../../../services/funzione.service';
 
 @Component({
   selector: 'app-dati-accesso',
@@ -18,10 +19,14 @@ export class DatiAccessoComponent implements OnInit, OnChanges {
   inizioSessione = null;
   fineSessione = null;
   durataSessione = null;
+  listaFunzioniAbilitate = [];
 
-  constructor() { }
+  listaFunzioni: Array<any> = [];
+
+  constructor(private funzioneService: FunzioneService) { }
 
   ngOnInit(): void {
+    this.letturaFunzioni();
   }
 
   ngOnChanges(sc: SimpleChanges) {
@@ -32,10 +37,36 @@ export class DatiAccessoComponent implements OnInit, OnChanges {
       this.inizioSessione = this.getDataSessioneFormattata(this.accesso.inizioSessione);
       this.fineSessione = this.getDataSessioneFormattata(this.accesso.fineSessione);
       this.durataSessione = Utils.getDurataSessioneFormattata(this.accesso.durata);
+      this.listaFunzioniAbilitate = this.accesso.listaFunzioni;
     }
+  }
+
+  letturaFunzioni() {
+    this.listaFunzioni = [];
+    this.funzioneService.letturaFunzioni().subscribe(listaFunzioni => {
+      listaFunzioni.forEach(funzione => {
+        this.listaFunzioni.push({
+          value: funzione,
+          label: funzione.descrizione,
+          checked: false,
+          disabled: true
+        });
+      });
+      Utils.ordinaOpzioniSelect(this.listaFunzioni);
+    });
   }
 
   getDataSessioneFormattata(dataSessione: Date): string {
     return dataSessione ? moment(dataSessione).format('DD/MM/YYYY HH:mm:ss') : null;
+  }
+
+  isChecked(funzione: any): boolean {
+    let esito = false;
+    this.listaFunzioniAbilitate.forEach(funzioneAbilitata => {
+      if (funzione.value.descrizione === funzioneAbilitata.descrizione) {
+        esito = true;
+      }
+    });
+    return esito;
   }
 }
