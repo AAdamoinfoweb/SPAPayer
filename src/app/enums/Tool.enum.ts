3c4efa506d2e8f5b9e536a905b6d9308043c6985
Gianfranco Brescia <gbrescia70@gmail.com>
2020-11-25 17:42:42 +0100

    avanzamento sviluppo dettaglio rendicontazione

--- src/app/enums/Tool.enum.ts
+++ src/app/enums/Tool.enum.ts
@@ -5,6 +5,7 @@ export enum ToolEnum {
   EXPORT_PDF = 'export_pdf',
   EXPORT_XLS = 'export_xls',
   INSERT_CARRELLO = 'aggiungi_carrello',
-  VISUALIZE_STATISTICS = 'visualizza statistiche'
+  VISUALIZE_STATISTICS = 'visualizza statistiche',
+  PRINT_RT = 'stampa ricevuta telematica'
 }
 
