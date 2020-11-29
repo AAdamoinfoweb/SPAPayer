84aa4b78dd7a0e9a85c7245f94edc8b09aaaae2a
Gianfranco Brescia <gbrescia70@gmail.com>
2020-11-29 12:46:57 +0100

    aggiunto campo livelloIntegrazioneId a oggetto DettaglioPendenza

--- src/app/modules/main/model/transazione/DettaglioPendenza.ts
+++ src/app/modules/main/model/transazione/DettaglioPendenza.ts
@@ -2,6 +2,7 @@ import {CampoDettaglioTransazione} from '../bollettino/CampoDettaglioTransazione
 
 export class DettaglioPendenza {
   id: number;
+  livelloIntegrazioneId: number;
   enteImpositore: string;
   servizioNome: string;
   enteBeneficiario: string;
