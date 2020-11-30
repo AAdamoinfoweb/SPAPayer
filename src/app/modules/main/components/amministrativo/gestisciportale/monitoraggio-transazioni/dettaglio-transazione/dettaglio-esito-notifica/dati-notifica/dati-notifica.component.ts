64dd067cef03946b1087db1c313041310f333b1f
Gianfranco Brescia <gbrescia70@gmail.com>
2020-11-30 09:23:40 +0100

    avanzamento sviluppo pagina "dettaglio esito notifiche"

--- src/app/modules/main/components/amministrativo/gestisciportale/monitoraggio-transazioni/dettaglio-transazione/dettaglio-esito-notifica/dati-notifica/dati-notifica.component.ts
+++ src/app/modules/main/components/amministrativo/gestisciportale/monitoraggio-transazioni/dettaglio-transazione/dettaglio-esito-notifica/dati-notifica/dati-notifica.component.ts
@@ -1,15 +1,82 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {EsitoNotifica} from '../../../../../../../model/transazione/EsitoNotifica';
+import {ToolEnum} from '../../../../../../../../../enums/Tool.enum';
+import {Tabella} from '../../../../../../../model/tabella/Tabella';
+import {tipoColonna} from '../../../../../../../../../enums/TipoColonna.enum';
+import {tipoTabella} from '../../../../../../../../../enums/TipoTabella.enum';
+import {EsitoTransazione} from '../../../../../../../model/transazione/EsitoTransazione';
 
 @Component({
   selector: 'app-dati-notifica',
   templateUrl: './dati-notifica.component.html',
   styleUrls: ['./dati-notifica.component.scss']
 })
-export class DatiNotificaComponent implements OnInit {
+export class DatiNotificaComponent implements OnInit, OnChanges {
+
+  @Input() datiNotifica: EsitoNotifica;
+
+  enteImpositore = null;
+  esitoNotifica = null;
+  listaEsitiTransazione = [];
+
+  toolbarIcons = [
+    {type: ToolEnum.PRINT_COMMIT_MSG, tooltip: 'Stampa COMMIT MSG'}
+  ];
+
+  tableData: Tabella = {
+    rows: [],
+    cols: [
+      {field: 'numeroTentativi', header: 'Numero tentativi', type: tipoColonna.TESTO},
+      {field: 'data', header: 'Data ora', type: tipoColonna.TESTO},
+      {field: 'esito', header: 'Esito', type: tipoColonna.TESTO}
+    ],
+    dataKey: 'id.value',
+    tipoTabella: tipoTabella.CHECKBOX_SELECTION
+  };
+
+  righeSelezionate: any[];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(sc: SimpleChanges) {
+    if (sc.datiNotifica) {
+      this.enteImpositore = this.datiNotifica.enteImpositore;
+      this.esitoNotifica = this.datiNotifica.esitoNotifica;
+      this.listaEsitiTransazione = this.datiNotifica.listaEsitiTransazione;
+      this.impostaTabellaEsitiTransazione(this.listaEsitiTransazione);
+    }
+  }
+
+  impostaTabellaEsitiTransazione(listaEsitiTransazione: EsitoTransazione[]): void {
+    this.tableData.rows = [];
+    if (listaEsitiTransazione) {
+      listaEsitiTransazione.forEach(esitoTransazione => {
+        this.tableData.rows.push(this.creaRigaTabella(esitoTransazione));
+      });
+    }
+  }
+
+  creaRigaTabella(esitoTransazione: EsitoTransazione): any {
+    return {
+      numeroTentativi: {value: esitoTransazione.numeroTentativi},
+      data: {value: esitoTransazione.data},
+      esito: {value: esitoTransazione.esitoNome}
+    };
+  }
+
+  eseguiAzioni(azioneTool: ToolEnum): void {
+    // TODO implementare logica per stampa COMMIT MSG
+  }
+
+  getNumeroRecord(): string {
+    return 'Totale ' + this.tableData.rows.length + ' esiti transazione';
+  }
+
+  selezionaRigaTabella(righeSelezionate: any[]): void {
+    this.righeSelezionate = righeSelezionate;
   }
 
 }
--- src/app/modules/main/components/amministrativo/gestisciportale/monitoraggio-transazioni/dettaglio-transazione/dettaglio-esito-notifica/dettaglio-esito-notifica.component.ts
+++ src/app/modules/main/components/amministrativo/gestisciportale/monitoraggio-transazioni/dettaglio-transazione/dettaglio-esito-notifica/dettaglio-esito-notifica.component.ts
@@ -1,15 +1,68 @@
 import {Component, OnInit} from '@angular/core';
+import {FormElementoParentComponent} from '../../../../form-elemento-parent.component';
+import {FunzioneGestioneEnum} from '../../../../../../../../enums/funzioneGestione.enum';
+import {EsitoNotifica} from '../../../../../../model/transazione/EsitoNotifica';
+import {ConfirmationService} from 'primeng/api';
+import {ActivatedRoute, ActivatedRouteSnapshot, Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {AmministrativoService} from '../../../../../../../../services/amministrativo.service';
+import {MonitoraggioTransazioniService} from '../../../../../../../../services/monitoraggio-transazioni.service';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-dettaglio-esito-notifica',
   templateUrl: './dettaglio-esito-notifica.component.html',
   styleUrls: ['./dettaglio-esito-notifica.component.scss']
 })
-export class DettaglioEsitoNotificaComponent implements OnInit {
+export class DettaglioEsitoNotificaComponent extends FormElementoParentComponent implements OnInit {
 
-  constructor() { }
+  FunzioneGestioneEnum = FunzioneGestioneEnum;
+  funzione: FunzioneGestioneEnum = FunzioneGestioneEnum.DETTAGLIO;
+
+  datiNotifica: EsitoNotifica = new EsitoNotifica();
+
+  titoloPagina = 'Dettaglio Esito Notifica';
+  tooltip = 'In questa pagina puoi visualizzare l\'esito delle notifiche inviate al portale esterno';
+
+  breadcrumbList = [];
+
+  constructor(confirmationService: ConfirmationService, protected router: Router, http: HttpClient,
+              protected amministrativoService: AmministrativoService, protected activatedRoute: ActivatedRoute,
+              private monitoraggioTransazioniService: MonitoraggioTransazioniService, private location: Location) {
+    super(confirmationService, activatedRoute, amministrativoService, http, router);
+  }
+
+  initFormPage(snapshot: ActivatedRouteSnapshot) {
+  }
 
   ngOnInit(): void {
+    this.breadcrumbList = this.inizializzaBreadcrumbList([
+      {label: 'Monitoraggio Transazioni', link: this.basePath},
+      {label: 'Dettaglio Transazione', link: null},
+      {label: 'Dettaglio Esito Notifica', link: null}
+    ], true);
+
+    const idSelezionato = parseInt(this.activatedRoute.snapshot.paramMap.get('transazioneId'));
+    this.monitoraggioTransazioniService.dettaglioEsitoNotifica(idSelezionato, this.idFunzione).subscribe(datiNotifica => {
+      this.datiNotifica = datiNotifica;
+    });
+
+    window.scrollTo(0, 0);
+  }
+
+  redirect(index, link): void {
+    if (index === 3) {
+      this.tornaIndietro();
+    } else {
+      this.router.navigateByUrl(link);
+    }
+  }
+
+  tornaIndietro(): void {
+    this.location.back();
+  }
+
+  onClickSalva(): void {
   }
 
 }
