cfa16794df6bdba234abb51cfe25e694bfa1a6e8
sante stanisci <s.sante@hotmail.it>
2020-11-30 17:32:38 +0100

    fix breadcrumb

--- src/app/modules/main/components/amministrativo/gestisci-servizi/form-servizio/form-servizio.component.ts
+++ src/app/modules/main/components/amministrativo/gestisci-servizi/form-servizio/form-servizio.component.ts
@@ -329,7 +329,7 @@ export class FormServizioComponent extends FormElementoParentComponent implement
   inizializzaBreadcrumb(): void {
     const breadcrumbs: SintesiBreadcrumb[] = [];
     breadcrumbs.push(new SintesiBreadcrumb('Gestisci Anagrafiche', null));
-    breadcrumbs.push(new SintesiBreadcrumb('Gestisci Servizio', this.basePath));
+    breadcrumbs.push(new SintesiBreadcrumb('Configura Servizi', this.basePath));
     breadcrumbs.push(new SintesiBreadcrumb(this.getTestoFunzione(this.funzione) + ' Servizio', null));
     this.breadcrumbList = this.inizializzaBreadcrumbList(breadcrumbs);
   }
