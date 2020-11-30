baed372f2747361a91e423dbfaf4facfbb1bc230
Gianfranco Brescia <gbrescia70@gmail.com>
2020-11-30 09:20:11 +0100

    aggiunta icona azione "stampa Commit Msg" in toolbar

--- src/app/enums/Tool.enum.ts
+++ src/app/enums/Tool.enum.ts
@@ -7,6 +7,7 @@ export enum ToolEnum {
   INSERT_CARRELLO = 'aggiungi_carrello',
   VISUALIZE_STATISTICS = 'visualizza statistiche',
   PRINT_RT = 'stampa ricevuta telematica',
-  PRINT_RPT = 'stampa ricevuta di pagamento telematica'
+  PRINT_RPT = 'stampa ricevuta di pagamento telematica',
+  PRINT_COMMIT_MSG = 'stampa Commit Msg'
 }
 
