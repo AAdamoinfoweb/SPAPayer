1edf11a7d3ec66509ab362fa335840a8c9e5880d
Ivan Lamparelli <lamparelli.ivan@gmail.com>
2020-12-09 11:52:58 +0100

    init logica amministrativa di quadratura

--- src/app/modules/main/components/amministrativo/gestisciportale/quadratura/quadratura.component.ts
+++ src/app/modules/main/components/amministrativo/gestisciportale/quadratura/quadratura.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import {GestisciElementoComponent} from '../../gestisci-elemento.component';
+import {ActivatedRoute, Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {AmministrativoService} from '../../../../../../services/amministrativo.service';
+import {Tabella} from '../../../../model/tabella/Tabella';
+import {ToolEnum} from '../../../../../../enums/Tool.enum';
+import {Colonna} from '../../../../model/tabella/Colonna';
+import {ImmaginePdf} from '../../../../model/tabella/ImmaginePdf';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-quadratura',
   templateUrl: './quadratura.component.html',
   styleUrls: ['./quadratura.component.scss']
 })
-export class QuadraturaComponent implements OnInit {
+export class QuadraturaComponent extends GestisciElementoComponent implements OnInit {
 
-  constructor() { }
+  filtriRicerca: any;
+  righeSelezionate: any[];
+  tableData: Tabella;
+
+  constructor(router: Router,
+              route: ActivatedRoute, protected http: HttpClient,
+              amministrativoService: AmministrativoService
+  ) {
+    super(router, route, http, amministrativoService);
+  }
 
   ngOnInit(): void {
+  }
+
+  callbackPopolaLista() {
+  }
+
+  creaRigaTabella(elemento: any) {
+  }
+
+  eseguiAzioni(azioneTool: ToolEnum): void {
+  }
+
+  getColonneFileExcel(colonne: Colonna[]): Colonna[] {
+    return [];
+  }
+
+  getColonneFilePdf(colonne: Colonna[]): Colonna[] {
+    return [];
+  }
+
+  getImmaginiFilePdf(righe?: any[]): ImmaginePdf[] | any[] {
+    return undefined;
+  }
+
+  getNumeroRecord(): string {
+    return '';
+  }
+
+  getObservableFunzioneRicerca(): Observable<any> {
+    return undefined;
+  }
+
+  getRigheFileExcel(righe: any[]): any[] {
+    return [];
+  }
+
+  getRigheFilePdf(righe: any[]): any[] {
+    return [];
+  }
+
+  selezionaRigaTabella(righeSelezionate: any[]): void {
   }
 
 }
