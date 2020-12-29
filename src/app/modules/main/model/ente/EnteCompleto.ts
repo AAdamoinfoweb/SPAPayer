e75b18938f3d431d6ba27647e1a680ab06f8b187
Ivan Lamparelli <lamparelli.ivan@gmail.com>
2020-12-29 11:07:47 +0100

    aggiornato model entecompleto in seguito a cambio backend

--- src/app/modules/main/model/ente/EnteCompleto.ts
+++ src/app/modules/main/model/ente/EnteCompleto.ts
@@ -1,6 +1,7 @@
 import {Beneficiario} from "./Beneficiario";
 import {Logo} from "./Logo";
 import {FlussoRiversamentoPagoPA} from "../servizio/FlussoRiversamentoPagoPA";
+import {CredenzialiApi} from './CredenzialiApi';
 
 export class EnteCompleto {
   id: number;
@@ -17,7 +18,6 @@ export class EnteCompleto {
   emailReferente: string;
   telefonoReferente: string;
   listaBeneficiari: Beneficiario[];
-  chiaveApi: string;
-  secretApi: string;
+  credenzialiApi: CredenzialiApi = new CredenzialiApi();
   flussoRiversamentoPagoPA: FlussoRiversamentoPagoPA;
 }
